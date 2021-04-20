be64f2d685119cc8c943869cef1865d38e0218cc
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2021-04-19 22:30:57 -0300

    Create blobs.js

---
+++ blobs.js
@@ -0,0 +1,25 @@
+/*var n = parseInt();
+var kg = parseFloat()
+
+while (n > 0) { //complete o codigo
+    var c = parseFloat();
+    let dias = gets(); //atribua o valor correto a variavel
+    while (kg > 1) { // complete o while
+        let kg = kg / 2
+        dias++
+    }
+}
+console.log(n + ` dias`); //complete o código para a saída*/
+
+var n = parseInt();
+var kg = parseFloat(300.00)
+
+while (n > 10) { //complete o codigo
+    var c = parseFloat();
+    let dias = gets(); //atribua o valor correto a variavel
+    while (kg > 1) { // complete o while
+        let kg = kg / 2
+        dias++
+    }
+}
+console.log(n + ` dias`); //complete o código para a saída
